50cab243ba80bbc59a0d5058b5491793ab9151c1
David Mellons <dmellons@gmail.com>
2024-01-28 22:42:16 -0800

    Teacher Create working

--- pages/api/teacher.ts
+++ pages/api/teacher.ts
@@ -23,7 +23,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                     lastName: lastName,
                     seid: seid,
                     caltidesNumId: seid,
-
                 }
             });
 
