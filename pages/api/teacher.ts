750b40142e49fd3157050be6a25ce67584c138d6
David Mellons <dmellons@gmail.com>
2024-01-28 22:38:37 -0800

    Made SEID on Teacher required and unique

--- pages/api/teacher.ts
+++ pages/api/teacher.ts
@@ -1,68 +1,87 @@
 import { PrismaClient, Teacher, Section, TeacherCredential } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { notFound } from 'next/navigation';
-import { TeacherCreate}
 
 export interface TeacherCardType extends Teacher {
-  sections: Section[];
-  credentials: TeacherCredential[];
+    sections: Section[];
+    credentials: TeacherCredential[];
 }
 
 const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    
+
     if (req.method === 'POST') {
+
         const firstName = req.query.firstName
         const lastName = req.query.lastName
+        const seid = req.query.seid
+        try {
+            const teacher = await prisma.teacher.create({
+                data: {
+                    firstName: firstName,
+                    lastName: lastName,
+                    seid: seid,
+                    caltidesNumId: seid,
 
-
-        const teacher = await prisma.teacher.create({
-            data:{
-                firstName: firstName,
-                
-            }
-        })
-    }
-  
-    if (req.method === 'GET') {
-    try {
-      const seid = req.query.seid as string;
-    
-      const teacher = await prisma.teacher.findUnique({
-        where: {
-            seid 
-        }, 
-        include: {
-            sections: {
-                include: {
-                    course: {
-                        include: {
-                            authTableId: true
-                        }
-                    }
                 }
-            }, 
-            credentials: true
-        }
-      })
+            });
 
-      if (!teacher) {
-        notFound();
-      }
-      res.status(200).json(teacher);
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
+            if (!teacher) {
+                notFound();
+            }
+
+            res.status(200).json(teacher)
+        }
+        catch (error) {
+            if (error.code === 'P2002') {
+                res.status(500).json({
+                    error: error,
+                    message: `${error.meta.target} already found. Please enter a unique value`
+                });
+            }
+            res.status(500).json({ error: error });
+        }
     }
-  } else {
-    res.status(405).end(); // Method Not Allowed
-  }
+
+    else if (req.method === 'GET') {
+        try {
+            const seid = req.query.seid as string;
+
+            const teacher = await prisma.teacher.findUnique({
+                where: {
+                    seid
+                },
+                include: {
+                    sections: {
+                        include: {
+                            course: {
+                                include: {
+                                    authTableId: true
+                                }
+                            }
+                        }
+                    },
+                    credentials: true
+                }
+            })
+
+            if (!teacher) {
+                notFound();
+            }
+            res.status(200).json(teacher);
+        } catch (error) {
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    } else {
+        res.status(405).end(); // Method Not Allowed
+    }
 }
 
 
 export const config = {
-  // api: {
-  //   responseLimit: false,
-  // }
+    // api: {
+    //   responseLimit: false,
+    // }
 }
 // export { handler as POST}
