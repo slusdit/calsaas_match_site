763fbdc7dcf97cedbb19a3a48caef1e4180d52a3
David Mellons <dmellons@gmail.com>
2024-06-18 10:37:45 -0700

    fixed footer covering datatable page controls

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default async function RootLayout({
             disableTransitionOnChange
           >
           <MainHeader />
-            <main className="mt-(-10)">
+            <main className="full mb-12">
               {session ? children :
                 <UnauthorizedButton
                   home
